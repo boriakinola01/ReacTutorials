4c96a07d583190064e9ab753ffdfbb148b16e4da
Bori <boriakinola01@gmail.com>
2024-06-27 10:20:04 +0100

    simplify app

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          You need to shut up blud
         </p>
         <a
           className="App-link"
