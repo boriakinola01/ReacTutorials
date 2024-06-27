8662f762a2b4796d332f307280e425d5f2ff32e0
Bori <boriakinola01@gmail.com>
2024-06-27 10:26:43 +0100

    new stuff

---
+++ first-app/src/NInjas.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Ninjas extends Component {
+    render(){
+        return(
+            <div className="ninja">
+                <div>Name: Bori</div>
+                <div>Age: 22</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
