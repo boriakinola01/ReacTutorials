c352789d32333fc22f06aacefef956ae9cf2efc5
Bori <boriakinola01@gmail.com>
2024-06-27 14:51:29 +0100

    props and lists

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,6 +1,16 @@
 import Ninjas from "./NInjas";
+import React, { useState } from 'react';
 
 function App() {
+
+  const [ninjas, setNinjas] = useState([
+    {name: 'Bori', age: 22, id: 1},
+    {name: 'Tishe', age: 21, id: 2},
+    {name: 'Enoch', age: 21, id: 3},
+    {name: 'Najat', age: 21, id: 4},
+    {name: 'Risso', age: 22, id: 5}
+  ]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -8,7 +18,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           You need to shut up blud
         </p>
-        <Ninjas/>
+        <Ninjas ninjas={ ninjas }/>
+      
       </header>
     </div>
   );
--- first-app/src/NInjas.js
+++ first-app/src/NInjas.js
@@ -1,14 +1,22 @@
 import { Component } from "react";
 
-class Ninjas extends Component {
-    render(){
-        return(
-            <div className="ninja">
-                <div>Name: Bori</div>
-                <div>Age: 22</div>
+function Ninjas({ ninjas }){
+
+    const ninjaList = ninjas.map(ninja => {
+        return (
+            <div className="ninja" key={ ninja.id } >
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
             </div>
         )
-    }
+    })
+
+    return(
+        <div className="ninja-list">
+            { ninjaList }
+        </div>
+    )
+    
 }
 
 export default Ninjas
